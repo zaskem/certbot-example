9c292959861a4f91075812728afc59bc2d90d00e
Matt Zaske <zaskem@mzonline.com>
2020-12-06 15:28:09 -0600

    Fixed script pointer to auth/cleanup scripts provided.

--- certbot-superdomain.sh
+++ certbot-superdomain.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-certbot certonly --manual --manual-public-ip-logging-ok --preferred-challenges=http --manual-auth-hook ./auth/auth-superdomain.sh --manual-cleanup-hook ./cleanup/cleanup-superdomain.sh -d superdomain.net -d www.superdomain.net
+certbot certonly --manual --manual-public-ip-logging-ok --preferred-challenges=http --manual-auth-hook ./auth/auth-host.sh --manual-cleanup-hook ./cleanup/cleanup-host.sh -d superdomain.net -d www.superdomain.net
 read -n1 -r -s -p "Press any key to load certificate...`echo $'\r\n\r\n'`"
 more /etc/letsencrypt/live/superdomain.net/cert.pem
 read -n1 -r -s -p "Press any key to load private key...`echo $'\r\n\r\n'`"
